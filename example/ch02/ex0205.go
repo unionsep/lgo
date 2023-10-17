578f226578e8e1e0cf834548b0d1f90a57133896
Bando Masaya <union.septem@gmail.com>
2023-10-17 15:43:29 +0900

    ch02追加

---
+++ example/ch02/ex0205.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	_0 := 0_0
+	_𝟙 := 20
+	π := 3
+	ａ := "hello"
+	数値１ := 33
+
+	fmt.Println(_0)
+	fmt.Println(_𝟙)
+	fmt.Println(π)
+	fmt.Println(ａ)
+	fmt.Println(数値１)
+}
---
+++ example/ch02/ex0206.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	ａ := "こんにちは"
+	a := "サヨウナラ"
+
+	fmt.Println(ａ)
+	fmt.Println(a)
+}
